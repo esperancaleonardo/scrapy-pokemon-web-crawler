52140ba6a49514bc66d431a5eccb54b3efab681c
Leonardo <leonardo.oliveira.esperanca@gmail.com>
2022-06-29 01:32:20 -0300

    base pokemon spider code

--- crawler/spiders/pokemon.py
+++ crawler/spiders/pokemon.py
@@ -4,7 +4,21 @@ import scrapy
 class PokemonSpider(scrapy.Spider):
     name = 'pokemon'
     allowed_domains = ['pokeapi.co']
-    start_urls = ['http://pokeapi.co/']
+    start_urls = ['http://pokeapi.co/api/v2/pokemon/ditto', 'http://pokeapi.co/api/v2/pokemon/pikachu']
 
     def parse(self, response):
-        pass
+        response = response.json()
+
+        yield {
+            'id': response['id'],
+            'name': response['name'],
+            'order': response['order'],
+            'base_experience': response['base_experience'],
+            'sprite': response['sprites']['front_default'],
+            'hp': response['stats'][0]['base_stat'],
+            'attack': response['stats'][1]['base_stat'],
+            'defense': response['stats'][2]['base_stat'],
+            'speed': response['stats'][5]['base_stat'],
+            'height': response['height'],
+            'weight': response['weight']
+        }
